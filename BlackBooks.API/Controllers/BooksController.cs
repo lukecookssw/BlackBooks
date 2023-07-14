417db3e3a7a17df107b067208f716e9084608362
Luke Cook [SSW] <100659715+lukecookssw@users.noreply.github.com>
2023-07-14 19:14:07 +1000

    Add project files.

---
+++ BlackBooks.API/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlackBooks.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly ILogger<BooksController> _logger;
+
+    public BooksController(ILogger<BooksController> logger)
+    {
+        this._logger = logger;
+    }
+    
+    [HttpGet("All")]
+    public ActionResult<List<string>> GetAll()
+    {
+        return new List<string>
+        { 
+            "Book 1", 
+            "Book 2",
+            "Book 3",
+            "Book 4"
+        };
+    }
+
+    [HttpGet("Search/{searchTerm}")]
+    public ActionResult<List<string>> Search(string searchTerm)
+    {
+        return new List<string>
+        { 
+            $"Book 1 containing { searchTerm }", 
+            $"Book 2 containing { searchTerm }"
+        };
+    }
+}
